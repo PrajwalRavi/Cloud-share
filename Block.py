841f080a7428d2b244b2916c6b4058dce9bfbd25
PrajwalRavi <prjwl132@gmail.com>
2019-02-23 16:28:18 +0530

    Added Transaction class

--- Block.py
+++ Block.py
@@ -3,47 +3,54 @@ import hashlib
 
 
 class User:
-
     def __init__(self, uid, uname, uage):
-        self.Uid = uid
-        self.Uname = uname
-        self.Uage = uage
-
+        self.u_id = uid
+        self.u_name = uname
+        self.u_age = uage
 
 
 class Block:
-    def __init__(self, timestamp, data, prev_hash=""):
-        # self.index = index
+    def __init__(self, timestamp, pending_transactions, prev_hash=""):
         self.timestamp = timestamp
-        self.data = data
+        self.pending_transactions = pending_transactions
         self.prev_hash = prev_hash
         self.nonce = 0
         self.hash = self.calculate_hash()
-        self.minerid  = None
+        self.miner_id = None
+        self.owners = {}
 
     def calculate_hash(self):
-        return str(hashlib.sha256(
-            (self.data + self.prev_hash + str(self.timestamp) + str(
-                self.nonce)).encode()).hexdigest())
+        str_transactions = ""
+        for trans in self.pending_transactions:
+            str_transactions += str(trans)
 
-    def mine_block(self, difficulty,uid):
+        return str(hashlib.sha256(
+            (str_transactions + self.prev_hash + str(self.timestamp) + str(self.nonce)).encode()).hexdigest())
+
+    def mine_block(self, difficulty, uid):
         while self.hash[0:difficulty] != '0' * difficulty:
             self.nonce += 1
             self.hash = self.calculate_hash()
-        self.minerid = uid
+
+        # Owner and the miner to be paid on purchase of the transaction
+        self.miner_id = uid
+        for transaction in self.pending_transactions:
+            self.owners[transaction] = transaction.owner_id
 
 
 class Transaction:
-    def __init__(self,table_ptr,description,data_cost,rewardee_list):
-        pass
+    def __init__(self, table_ptr, description, data_cost, owner_id):
+        self.owner_id = owner_id
+        self.table_ptr = table_ptr
+        self.description = description
+        self.data_cost = data_cost
 
 
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
-        self.difficulty = 5
+        self.difficulty = 4
         self.pending_transactions = []
-        self.mining_reward = 100
 
     @staticmethod
     def create_genesis_block():
@@ -52,24 +59,17 @@ class Blockchain:
     def get_latest_block(self):
         return self.chain[-1]
 
-    def mine_pending_transactions(self, rewardaddress, data): #reward address is the user id of the miner
-        old_block = self.get_latest_block()
-        new_block = Block(datetime.datetime.now(), self.pending_transactions, old_block.hash)
-        new_block.mine_block(self.difficulty,rewardaddress)
+    def mine_pending_transactions(self, reward_address):  # reward address is the user id of the miner
+        latest_block = self.get_latest_block()
+        new_block = Block(datetime.datetime.now(), self.pending_transactions, latest_block.hash)
+        new_block.mine_block(self.difficulty, reward_address)
 
         self.pending_transactions = []
-        # print(new_block.hash)
+
+        ############################
         # Zero knowledge proof
         self.chain.append(new_block)
-        #
-        # def add_data(self, data):
-        #     old_block = self.get_latest_block()
-        new_block = Block(datetime.datetime.now(), data, old_block.hash)
-
-    #     new_block.mine_block(self.difficulty)
-    #     print(new_block.hash)
-    #     # Zero knowledge proof
-    #     self.chain.append(new_block)
+        ############################
 
     def verify_chain(self):
         for i in range(1, len(self.chain)):
@@ -77,21 +77,22 @@ class Blockchain:
             prev_block = self.chain[i - 1]
 
             if curr_block.calculate_hash() != curr_block.hash or curr_block.prev_hash != prev_block.hash:
-                # 1st condn : Checks current block's integrity
-                # 2nd condn : If a user changes data of a block and calls calculate_hash(), the previous condn cannot
+                # 1st cond : Checks current block's integrity
+                # 2nd cond : If a user changes data of a block and calls calculate_hash(), the previous condn cannot
                 return False
 
         return True
         # catch it , but this condition catches it
 
-    def add_data(self, data):
-        trans_obj = Transaction(data)
+    def add_data(self, description, table_ptr, cost, user_id):
+        trans_obj = Transaction(table_ptr, description, cost, user_id)
         self.pending_transactions.append(trans_obj)
-        pass
 
 
 blockchain = Blockchain()
 print("Mining block 1 ........")
-blockchain.add_data("Prajwal")
+blockchain.add_data("Prajwal", "vsfgd", 60, 5)
 print("Mining block 2 ........")
-blockchain.add_data("Athilesh")
+blockchain.add_data("Athilesh", "vsgfdg", 56, 6)
+blockchain.mine_pending_transactions(5)
+print(blockchain.chain)
